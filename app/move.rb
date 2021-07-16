b98fa5c1493f3fe92c38e9b6258884ad63040886
justin <justin-@users.noreply.github.com>
2021-07-16 12:34:06 -0400

    Avoid other snakes

--- app/move.rb
+++ app/move.rb
@@ -2,15 +2,15 @@ def move(params)
   @board    = params[:board]
   @my_snake = params[:you]
   @my_head  = @my_snake[:head]
+  @all_snakes = params[:board][:snakes]
 
   move =
-    avoid_wall(@board, @my_snake, @my_head)
+    do_the_math(@board, @my_snake, @my_head, @all_snakes)
 
-  puts "MOVE: " + move
   { "move": move }
 end
 
-def avoid_wall(board, my_snake, my_head)
+def do_the_math(board, my_snake, my_head, all_snakes)
   possible_moves = {
     left:  { x: my_head[:x] - 1 },
     right: { x: my_head[:x] + 1 },
@@ -18,18 +18,40 @@ def avoid_wall(board, my_snake, my_head)
     down:  { y: my_head[:y] - 1 }
   }
 
+  possible_moves.each do |direction, delta|
+    proposed_position = my_head.merge(delta)
+
+    next if self?(my_snake, proposed_position)
+    next if wall?(board, proposed_position)
+    next if other_snake?(all_snakes, proposed_position)
+
+    return direction.to_s
+  end
+end
+
+def self?(my_snake, position)
+  my_snake[:body].include?(position)
+end
+
+def wall?(board, position)
   board_boundaries = {
     x: { min: 0, max: board[:width] - 1 },
     y: { min: 0, max: board[:height] - 1 }
   }
 
-  possible_moves.each do |direction, delta|
-    proposed_position = my_head.merge(delta)
-
-    if ((proposed_position[:x] < board_boundaries[:x][:max]) && (proposed_position[:y] < board_boundaries[:y][:max])) &&
-        ((proposed_position[:x] > board_boundaries[:x][:min]) && (proposed_position[:y] > board_boundaries[:y][:min])) &&
-        !my_snake[:body].include?(proposed_position)
-      return direction.to_s
-    end
+  if ((position[:x] < board_boundaries[:x][:max]) && (position[:y] < board_boundaries[:y][:max])) &&
+      ((position[:x] > board_boundaries[:x][:min]) && (position[:y] > board_boundaries[:y][:min]))
+    false
+  else
+    true
   end
+end
+
+# TODO: Remove self
+def other_snake?(snakes, position)
+  snakes.each do |snake|
+    return true if snake[:body].include?(position)
+  end
+
+  false
 end
